188f603f03e13108f709a3a8aa82d955ead4fe42
Samsul Hadi <samsulhadikediri@gmail.com>
2020-06-02 18:49:52 +0700

    Initial Commit

---
+++ My First App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My First App
+//
+//  Created by SAMSUL HADI on 02/06/20.
+//  Copyright © 2020 SAMSUL HADI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
